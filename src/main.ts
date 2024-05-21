28f41235db83493729ae42da53b1386148515812
Albert Cheng <aabb9052@gmail.com>
2024-05-22 01:18:05 +0800

    feat: add vuetify theme and mdi icons

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,17 @@
 import './assets/main.scss'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify.ts'
 
 const app = createApp(App)
-const vuetify = createVuetify({
-  components,
-  directives
-})
 
 app.use(createPinia())
 app.use(router)
