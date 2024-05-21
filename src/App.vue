5cb80b08d5d3463060f0f27eb6b796e57bda6fbd
Albert Cheng <aabb9052@gmail.com>
2024-05-22 01:21:35 +0800

    feat: add two stores and finish the drawer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import { useLinksStore } from '@/stores/links'
+import { useUserStore } from '@/stores/user'
+
+const linksStore = useLinksStore()
+const userStore = useUserStore()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <v-app>
+    <v-navigation-drawer expand-on-hover rail permanent>
+      <template v-slot:prepend>
+        <v-list>
+          <v-list-item prepend-icon="mdi-book">
+            <v-list-item-title class="text-h4 font-weight-bold">DocCenter</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </template>
+      <v-divider thickness="2" color="primary"></v-divider>
+      <v-list dense>
+        <v-list-item
+          v-for="item in linksStore.links"
+          :title="item.title"
+          :key="item.title"
+          :to="item.url"
+          :prepend-icon="item.icon"
+        >
+        </v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <v-list>
+          <v-list-item
+            :prepend-avatar="userStore.user.avatar"
+            :subtitle="userStore.user.email"
+            :title="userStore.user.name"
+          >
+          </v-list-item>
+        </v-list>
+      </template>
+    </v-navigation-drawer>
+    <RouterView />
+  </v-app>
 </template>
