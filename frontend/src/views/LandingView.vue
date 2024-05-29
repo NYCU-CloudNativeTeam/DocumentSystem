28279dbb25196bc5ce445b89d6190610fa928054
SL510457 <sherryqq.lee@gmail.com>
2024-05-29 20:55:27 +0800

    feat: create landing page

---
+++ frontend/src/views/LandingView.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col lg="6">
+        <h2 class="intro-text">Welcome to the Document Center</h2>
+        <!-- <p class="description">Hi</p> -->
+        <v-btn color = "indigo" class="white--text fixed-button" rounded >
+          Get Started 
+          <v-icon right>mdi-arrow-right</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col lg="6">
+        <v-img src="/discussion.png"></v-img>
+      </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+ h2.intro-text {
+   font-size: 4rem;
+   font-weight: 700;
+   margin-bottom: 1rem;
+   
+ }
+ .v-col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.fixed-button {
+  width: 170px;
+  height: 50px;
+}
+</style>
